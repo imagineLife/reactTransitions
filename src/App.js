1ece1c6a2c592236aa0f1e613296be4ae658dd99
Eric Laursen <ericjlaursen@gmail.com>
2018-09-18 21:02:05 -0400

    Simplifies start.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import Ball from "./Ball";
 
 class App extends React.Component {
-  constructor(props){
-
-  	super(props);
-
-  	this.state = {
-      ballLeft : true
-	  };
-
-    // this.ballJump = this.ballJump.bind(this);
-  };
-
-  const test = 'test';
 
   render() {
-    const { ballLeft } = this.state;
     return (
-      <div>
-        <svg style={{ width: "300", height: "300px" }} onClick={this.ballJump}>
-          <Ball x={ballLeft ? 15 : 250} />
-        </svg>
-      </div>
+      <React.Fragment>
+        <h2>Begin!</h2>
+        <p>Test hot-reloading</p>
+      </React.Fragment>
     );
   }
 }
