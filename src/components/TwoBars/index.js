991eb2c454569a7f37e1a391003eb21cb37a8ebd
Eric Laursen <ericjlaursen@gmail.com>
2018-09-19 20:58:47 -0400

    Dummy component text

--- src/components/TwoBars/index.js
+++ src/components/TwoBars/index.js
@@ -1,31 +1,13 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import './index.css';
 
-export function ToolBar(props) {
-    let folderList = [
-        {
-            id: 'link1',
-            name: 'ToDo List',
-        }
-    ]
-    const folders = folderList.map(folder =>
-        <li key={folder.id} className="folder-menu-list-item">
-            <Link to={`/${folder.id}`}>
-                {folder.name}
-            </Link>
-        </li>
-    );
+export function TwoBars() {
 
     return (
-        <div className="sidebar sidebar-left">
-            <nav className="folder-menu">
-                <ul className="folder-menu-list">
-                    {folders}
-                </ul>
-            </nav>
+        <div className="twoBarsWrapper">
+            <p>Two Bars here!</p>
         </div>
     );
 }
 
-export default ToolBar;
+export default TwoBars;
