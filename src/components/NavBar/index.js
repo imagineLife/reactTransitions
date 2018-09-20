e30a20e49934a4adf70eb45847b3f272515a46a2
Eric Laursen <ericjlaursen@gmail.com>
2018-09-19 20:56:53 -0400

    Updates styles.

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -5,21 +5,19 @@ import './index.css';
 export function ToolBar(props) {
 
     const links = props.links.map(l =>
-        <li key={l.text} className="folder-menu-list-item">
-            <Link to={`${l.url}`}>
+        <li key={l.text} className="navLinkLi">
+            <Link className="link" to={`${l.url}`}>
                 {l.text}
             </Link>
         </li>
     );
 
     return (
-        <div className="sidebar sidebar-left">
-            <nav className="folder-menu">
-                <ul className="folder-menu-list">
-                    {links}
-                </ul>
-            </nav>
-        </div>
+        <nav className="nav">
+            <ul className="navList">
+                {links}
+            </ul>
+        </nav>
     );
 }
 
