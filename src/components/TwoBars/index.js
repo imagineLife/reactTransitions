ee994f519c3e49c591b7c933dcd1af654b01ad09
Eric Laursen <ericjlaursen@gmail.com>
2018-09-25 15:23:23 -0400

    Updates 2 bars on button click.

--- src/components/TwoBars/index.js
+++ src/components/TwoBars/index.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import './index.css';
+import * as d3 from 'd3'
+import Rect from '../Rect'
+export default class TwoBars extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			toggled: false
+		}
+		
+		this.toggleBars = this.toggleBars.bind(this)
+	}
 
-export function TwoBars() {
+	toggleBars(){
+		console.log('toggling bars!')
+		this.setState({toggled: !this.state.toggled})
+	}
 
-    return (
-        <div className="twoBarsWrapper">
-            <p>Two Bars here!</p>
-        </div>
-    );
+	render(){
+		console.log('toggled?!')
+		console.log(this.state.toggled)
+		let xScale = d3.scaleLinear().domain([0,100]).range([50,650])
+		let yScale = d3.scaleLinear().domain([0,100]).range([400,50])
+		let dummyData = [
+			{
+				x:'100',
+				name:'first',
+				togglingH: ['200','300']
+			},
+			{
+				x:'300',
+				name:'second',
+				togglingH: ['300','200']
+			}
+		]
+
+		let rects = dummyData.map(d => {
+			return <Rect 
+				key={d.name}
+				x={d.x}
+				h={this.state.toggled ? d.togglingH[0] : d.togglingH[1]}
+			/>
+		})
+
+		return (
+			<React.Fragment>
+		        <svg className="twoBarsWrapper">
+		            {rects}
+		        </svg>
+		        <button onClick={this.toggleBars}>Animate Bars</button>
+	        </React.Fragment>
+	    );
+	}
 }
-
-export default TwoBars;
